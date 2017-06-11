dd849b00916e44e4f8aab9d29cf25d856c856c00
Gemba69 <bluhn@web.de>
2017-06-11 22:57:41 +0200

    fixed random outOfBoundException

--- src/main/java/de/pvr/fish/simulation/util/RandomGenerator.java
+++ src/main/java/de/pvr/fish/simulation/util/RandomGenerator.java
@@ -1,5 +1,6 @@
 package de.pvr.fish.simulation.util;
 
+import de.pvr.fish.simulation.config.FishParameter;
 import de.pvr.fish.simulation.model.Fish;
 import de.pvr.fish.simulation.model.Position;
 
@@ -22,17 +23,30 @@ public class RandomGenerator {
 		return new Position(getRandomCoordinate(length), getRandomCoordinate(height));
 	}
 	
+	public static Position getRandomNextPosition(Position p) {
+		double coordX = p.getCoordinateX() + getRandomRangeNumber();
+		double coordY = p.getCoordinateX() + getRandomRangeNumber();
+		while (coordX > FishParameter.FIELD_LENGTH || coordX < 0) {
+			coordX = p.getCoordinateX() + getRandomRangeNumber();
+		}
+		while (coordY > FishParameter.FIELD_HEIGHT || coordY < 0) {
+			coordY = p.getCoordinateY() + getRandomRangeNumber();
+		}
+		return new Position(coordX, coordY);
+	}
+	
 	public static int getRandomAngle() {
 		return (int) (Math.random() * 360 + 1);
 	}
 	
 	public static Fish getRandomFish(int length, int height) {
-		return new Fish(getRandomPosition(length, height), getRandomPosition(length + 2, height + 2));
-		//FIXME OutOfBounds
+		Position p = getRandomPosition(length, height);
+		return new Fish(p, getRandomNextPosition(p));
 	}
 	
 	public static int getRandomSpeed() {
 		return getRandomNumber(3);
+		//FIXME 2 eleganter machen mit den anderen auf Speed mappen
 	}
 	
 	public static int getRandomSpeed(int maxSpeed) {
@@ -46,4 +60,5 @@ public class RandomGenerator {
 	public static int getRandomRangeNumber(int number) {
 		return getRandomNumber(2 * number) - number;
 	}
+
 }
