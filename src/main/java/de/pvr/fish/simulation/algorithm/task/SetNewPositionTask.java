7b5c3b5373c89c22e0d24853cc69129e48761aac
Gemba69 <bluhn@web.de>
2017-06-16 12:30:00 +0200

    changed r1 behaviour

--- src/main/java/de/pvr/fish/simulation/algorithm/task/SetNewPositionTask.java
+++ src/main/java/de/pvr/fish/simulation/algorithm/task/SetNewPositionTask.java
@@ -1,7 +1,6 @@
 package de.pvr.fish.simulation.algorithm.task;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
