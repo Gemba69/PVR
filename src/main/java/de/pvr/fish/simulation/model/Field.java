693774d4225fe808352211abb6404ab83c020a35
Gemba69 <bluhn@web.de>
2017-06-16 16:49:50 +0200

    added logging methode for debugging

--- src/main/java/de/pvr/fish/simulation/model/Field.java
+++ src/main/java/de/pvr/fish/simulation/model/Field.java
@@ -11,36 +11,34 @@ import de.pvr.fish.simulation.algorithm.task.CalculatePositionTask;
 import de.pvr.fish.simulation.algorithm.task.FishTask;
 import de.pvr.fish.simulation.algorithm.task.SetNewPositionTask;
 
-
 public class Field {
-	
+
 	private ArrayList<Fish> fishes;
 	private int length;
 	private int height;
-	
+
 	private int threads;
 	private int fishNumber;
-	
+
 	private ExecutorService executorService;
 	private ArrayList<FishTask> calcTasks;
 	private ArrayList<FishTask> newPositionTasks;
-	
+
 	private static final Logger LOG = LogManager.getLogger(Field.class);
-	
-	
+
 	public Field(int length, int height, int fishNumber, int threads) {
 		this.length = length;
 		this.height = height;
 		this.fishNumber = fishNumber;
 		this.threads = threads;
 		fishes = new ArrayList<Fish>();
-		
+
 		this.executorService = Executors.newFixedThreadPool(threads);
 		this.calcTasks = new ArrayList<FishTask>();
 		this.newPositionTasks = new ArrayList<FishTask>();
-		
+
 	}
-	
+
 	public boolean addNewFishToField(Fish fish) {
 		if (fishes.contains(fish)) {
 			return false;
@@ -49,48 +47,59 @@ public class Field {
 			return true;
 		}
 	}
-	
+
 	public void nextInteration() {
 		LOG.info("Starting overall Iteration");
-		//Execution
+		// Execution
 		try {
 			this.executorService.invokeAll(this.calcTasks);
 		} catch (InterruptedException e) {
 			LOG.error(e.getMessage());
 		}
-		//2. Execution
+		// 2. Execution
 		try {
 			this.executorService.invokeAll(this.newPositionTasks);
 		} catch (InterruptedException e) {
 			LOG.error(e.getMessage());
 		}
+		logFishes(); // only fpr debugging
 	}
-	
+
 	public void prepareTaskLists() {
 		this.calcTasks.clear();
 		this.newPositionTasks.clear();
-		//split Task 
+		// split Task
 		int startPosition = 0;
 		ArrayList<Integer> positions = splitTasks();
 		for (Integer endPosition : positions) {
-			this.calcTasks.add(new CalculatePositionTask(this.fishes, new ArrayList<Fish>(this.fishes.subList(startPosition, endPosition)), startPosition, endPosition));
+			this.calcTasks.add(new CalculatePositionTask(this.fishes,
+					new ArrayList<Fish>(this.fishes.subList(startPosition, endPosition)), startPosition, endPosition));
 			startPosition = endPosition;
 		}
 		startPosition = 0;
 		for (Integer endPosition : positions) {
-			this.newPositionTasks.add(new SetNewPositionTask(this.fishes, new ArrayList<Fish>(this.fishes.subList(startPosition, endPosition)), startPosition, endPosition));
+			this.newPositionTasks.add(new SetNewPositionTask(this.fishes,
+					new ArrayList<Fish>(this.fishes.subList(startPosition, endPosition)), startPosition, endPosition));
 			startPosition = endPosition;
 		}
 	}
-	
+
 	public ArrayList<Integer> splitTasks() {
 		ArrayList<Integer> positions = new ArrayList<Integer>();
 		for (int i = 0; i < threads - 1; i++) {
-			positions.add(fishNumber/threads * (i + 1));
+			positions.add(fishNumber / threads * (i + 1));
 		}
 		positions.add(fishNumber);
 		return positions;
 
+	}
+
+	private void logFishes() {
+		for (Fish fish : this.fishes) {
+			LOG.debug("Fish at Position " + fish.getPosition().getCoordinateX() + "/"
+					+ fish.getPosition().getCoordinateY() + " with Angle " + fish.getAngle() + " and Speed "
+					+ fish.getNewSpeed());
+		}
 	}
 
 	public ArrayList<Fish> getFishes() {
@@ -132,7 +141,7 @@ public class Field {
 	public void setCalcTasks(ArrayList<FishTask> tasks) {
 		this.calcTasks = tasks;
 	}
-	
+
 	public ArrayList<FishTask> getNewPositionTasks() {
 		return newPositionTasks;
 	}
